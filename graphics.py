b265c206e08431eca77a0e6b12fc7995622262ba
Nikolay Nechaev <nikolay_nechaev@mail.ru>
2019-10-18 09:18:29 +0300

    Minor graphics improvements

--- graphics.py
+++ graphics.py
@@ -1,24 +1,29 @@
-from objects import * # Animal, Predator, Victim, ...
+from objects import Animal, Predator
 
 
 class Field():
-    # Объявим по дефолту пустую доп. инициализацию:
-    field_additional_init = lambda self: None
-
     def __init__(self, max_field_x, max_field_y):
-        self.max_field_x = max_field_x
-        self.max_field_y = max_field_y
-        self.predator = objects[Predator][0]
-        self.victims = objects[Victim]
-        '''
-        Initializes a `Field` object
+        """Initializes a `Field` object
                 Parameters:
                     max_field_x (integer, obligatory):
                         A x axis field size 
                     max_field_y (integer, obligatory):
                         A y axis field size   
-        '''
+        """
+        self.max_field_x = max_field_x
+        self.max_field_y = max_field_y
+        self.predator = objects[Predator][0]
+        self.victims = objects[Victim]
+
+    def update(self):
+        """This method update victims and predator's coordinates
+        """
+        self.predator = objects[Predator][0]
+        self.victims = objects[Victim]
+    
     def show(self):     #Перед вызовом необходимо обновить поле
+        """This method print field in ascii pseudographics 
+        """
         field = []
         for i in self.max_field_y:
             field.append('#'*self.max_field_x)      #Будем обозначать '#' пустую клетку
@@ -26,13 +31,3 @@ class Field():
         for victim in self.victims:
             field[victim.y][victim.x] = 'x'     #Будем обозначать 'x' жертв
         print(field)
-        '''
-            This method print field in ascii pseudographics 
-        '''
-
-    def update(self):
-        self.predator = objects[Predator][0]
-        self.victims = objects[Victim]
-        '''
-            This method update victims and predator's coordinates
-        '''
